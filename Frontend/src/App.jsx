e30f077a102a76e7cf0a5489022a8d7eb6d4dc88
Mohit Raut <mohitraut087@gmail.com>
2025-07-19 19:42:02 +0530

    fix

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./Pages/Home";
+import NotFound from "./Pages/NotFound";
+import Dashboard from "./Pages/Dashboard";
+import Header from "./Components/Header";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-      App
-    </>
-  )
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
-
-export default App
